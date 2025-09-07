c9f67941d199351e29aa45e3ee92b4f59901f0d7
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-09-07 14:44:17 -0500

    added search tools. needs improvement still

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,10 +1,10 @@
 import { getBusinesses } from "./database.js";
 
-const businessList = getBusinesses();
+const businesses = getBusinesses();
 
 export const BusinessList = () => {
   let businessHTML = ``;
-  businessList.forEach((business) => {
+  businesses.forEach((business) => {
     businessHTML += `<div class="business">
                         <h3><strong>${business.companyName}</strong></h3>
                         <p>${business.addressFullStreet}<br />
@@ -15,3 +15,31 @@ export const BusinessList = () => {
 
   return businessHTML;
 };
+
+document.querySelector("#content").addEventListener("keydown", (event) => {
+  if (event.target.id !== "companySearch") return;
+  if (event.key !== "Enter") return;
+
+  const companySearchResultArticle = document.querySelector(".foundBusinesses");
+
+  if (event.key === "Enter") {
+    const foundBusiness = businesses.find((business) =>
+      business.companyName
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase())
+    );
+
+    if (foundBusiness) {
+      companySearchResultArticle.innerHTML = `
+          <div class="business">
+            <h3><strong>${foundBusiness.companyName}</strong></h3>
+            <p>${foundBusiness.addressFullStreet}<br />
+            ${foundBusiness.addressCity}, ${foundBusiness.addressStateCode} ${foundBusiness.addressZipCode}</p>
+          </div>
+          <hr/>
+        `;
+    } else {
+      companySearchResultArticle.innerHTML = `<p>No business found</p>`;
+    }
+  }
+});
